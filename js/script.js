70f52d7bbb7101a70a969bb140a6865f60969fea
Yuana1101 <yuanaeka89@gmail.com>
2024-01-13 21:56:16 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const btnDarkMode = document.querySelector('button.btn-darkmode');
+const footer = document.querySelector('footer');
+
+btnDarkMode.addEventListener('click', () => {
+    document.body.classList.toggle('darkmode-active');
+    footer.classList.toggle('bg-darker-color');
+    btnDarkMode.classList.toggle('btn-darkmode-clicked');
+    document.querySelector('aside article header #contact').classList.toggle('darkmode-active');
+})
